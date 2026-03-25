7091552c626a12dd5a9ad77e64921b69e068bb68
ah5e <wwfawaz220@gmail.com>
2026-03-25 18:41:47 +0300

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,14 @@
+$zip = "$env:USERPROFILE\Desktop\VELARIS.zip"
+$folder = "$env:USERPROFILE\Desktop\VELARIS"
+
+Write-Host "Downloading VELARIS..." -ForegroundColor Cyan
+
+Invoke-WebRequest "https://drive.google.com/uc?export=download&id=1YC8A-AF1j16KrKq6_aitUEdPKRH34Vrt" -OutFile $zip
+
+Write-Host "Extracting..." -ForegroundColor Yellow
+
+Expand-Archive -Path $zip -DestinationPath $folder -Force
+
+Write-Host "Launching..." -ForegroundColor Green
+
+Start-Process "$folder\Velaris Premium.exe"
